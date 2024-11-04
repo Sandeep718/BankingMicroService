81b6fddca397719511ad70c73c578172d5987c76
sandeep <114744537+Sandeep718@users.noreply.github.com>
2024-11-04 16:55:20 +0530

    Update App.java

--- banking-microservices/src/main/java/com/sandeep/App.java
+++ banking-microservices/src/main/java/com/sandeep/App.java
@@ -5,6 +5,6 @@ package com.sandeep;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello banking-1");
+        System.out.println("Hello banking-2");
     }
 }
